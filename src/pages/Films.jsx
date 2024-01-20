cba31989bfbcceb2022bea32c0f0ad598d6b4a8a
aliciavb <152905690+aliciavb@users.noreply.github.com>
2024-01-20 18:33:07 +0100

    deleted unused links

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -35,7 +35,6 @@ export const Films = () => {
         <section className="Films-personal">
           <div className="Personal-intro">
             <h3>Your seen films</h3>
-            <span>See all</span>
           </div>
           <div className="Popular-wrapper Personal-wrapper">
             {/* botones prev y next para carrusel */}
@@ -45,7 +44,6 @@ export const Films = () => {
         <section className="Films-popular">
           <div className="Popular-intro">
             <h3>Popular films this week</h3>
-            <span>More</span>
           </div>
           <div className="Popular-wrapper">
             {/* botones prev y next para carrusel */}
@@ -59,56 +57,6 @@ export const Films = () => {
           </div>
         </section>
       </div>
-    );
-  };
+    )
+  }
   
-
-
-
-
-
-<>
-
-{/* <table cellpadding="0" cellspacing="0" id="diary-table" className="table film-table">
-	<thead>
-		<tr>
-		<th scope="col" className="th-film">Film</th>
-		<th scope="col" className="th-released center">Released</th>
-		<th scope="col" className="th-actions film-actions center">
-            <span className="shown-for-owner">Edit</span>
-        </th>
-		</tr>
-	</thead>
-	<tbody>
-	    <tr className="">
-            <td className="">
-                <div className="">
-                    <div>
-                        <img src="" width="35" height="52" alt="La La Land" className="image" />
-                            <a href="/aliverso/film/la-la-land/" className="frame">
-                                <span className="frame-title">La La Land (2016)</span>
-                                <span className="overlay"></span>
-                            </a>
-                    </div>
-                </div>
-                <h3 className="headline-3 prettify">
-                    <a href="/aliverso/film/la-la-land/">La La Land</a>
-                </h3>
-            </td>
-            
-            <td className=""><span>2016</span></td>
-        
-	        <td className="">
-            <span className="overlay-actions -w150 js-film-options">
-			<span className="diary-entry-edit shown-for-owner" data-owner="aliverso">
-            <a href="#" rel="nofollow" className="edit-review-button has-icon icon-16 icon-edit cboxElement" data-delete-viewing-url="/s/viewing:510671995/delete" data-viewing-id="510671995" data-film-id="240344" data-film-name="La La Land" data-film-poster="/film/la-la-land/image-150/" data-film-year="2016" data-viewing-date="2024-01-12" data-viewing-date-str="12 Jan 2024" data-review-text="" data-rating="2" data-tags="[  ]" data-rewatch="false" data-specified-date="true" data-contains-spoilers="false" data-spoilers-locked="false">
-            <span className="icon"></span>Edit this entry</a>
-            </span>
-			<span className="replace menu-link icon" style="opacity: 1;"></span>
-		    </span>
-            </td>
-        </tr>
-    </tbody>
-</table> */}
-
-</>
